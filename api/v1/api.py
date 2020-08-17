50c038fc189f345787b9e981545edb6464ae53e8
muslax <muslax@gmail.com>
2020-08-17 08:12:12 +0700

    Add license user operations

--- api/v1/api.py
+++ api/v1/api.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter
 
 from api.v1.admin_license import router as admin_license
 from api.v1.admin_user import router as admin_user
-# from api.v1.license import router as license_routes
-# from api.v1.user import router as user_routes
+from api.v1.license import router as license_routes
+from api.v1.user import router as user_routes
 # from api.v1.client import router as client_routes
 
 router = APIRouter()
@@ -11,6 +11,6 @@ router = APIRouter()
 router.include_router(admin_license, prefix="/admin/licenses", tags=["🅰️ Licenses"])
 router.include_router(admin_user, prefix="/admin/users", tags=["🅰️ Users"])
 
-# router.include_router(license_routes, prefix="/licenses", tags=["✅  Tenant"])
-# router.include_router(user_routes, prefix="/users/{license}", tags=["✅  Tenant Users"])
+router.include_router(license_routes, prefix="/licenses", tags=["✅  Tenant"])
+router.include_router(user_routes, prefix="/users/{license}", tags=["✅  Tenant Users"])
 # router.include_router(client_routes, prefix="/clients/{license}", tags=["✅  Tenant Clients"])
--- api/v1/user.py
+++ api/v1/user.py
@@ -5,76 +5,61 @@ from fastapi import APIRouter
 
 from crud import user as crud
 from crud.license import find_one as find_license
-from utils.utils import raise_not_found, raise_bad_request
 from models.base import Msg
 from models.user import User, UserCreate, UserUpdate
+from utils.utils import raise_bad_request, raise_not_found
 
 
 router = APIRouter()
 
 
 @router.get("", response_model=List[User])
-async def read_users(license: str = "all", limit: int=20, skip: int=0):
-    '''
-    License: `all` or license's `slug`.
-    '''
+async def read_license_users(license: str, limit: int=20, skip: int=0):
+    '''License: license's slug or * (all)'''
     logging.info(">>> " + __name__ + ":get")
     return await crud.find_many(license, limit, skip)
 
 
 @router.post("", response_model=User)
-async def create_license_owner(data: UserCreate):
-    '''
-    Required: `license, name, username, email, password`
-    '''
+async def create_license_user(data: UserCreate):
+    '''Required: `license, name, username, email, password`'''
     logging.info(">>> " + __name__ + ":create")
     license = await find_license(data.license)
+    logging.info(license)
     if not license:
-        raise_bad_request("Could not find license with the specified slug")
-
-    user = await crud.find_license_owner(data.license)
-    if user:
-        raise_bad_request("License owner user already exists in the system.")
-
+        raise_bad_request("License is not valid.")
     user = await crud.find_by_email_or_username(data.email, data.username)
     if user:
-        raise_bad_request('Email or username is already registered in the system.')
-
-    return await crud.insert_license_owner(data)
+        raise_bad_request("Username or email is already registered in the system.")
+    return await crud.insert_one(data, license_owner=False)
 
 
 @router.get("/{search}", response_model=User)
-async def find_user(search: str):
-    '''
-    Search: `id`, `username`, or `email`.
-    '''
+async def find_license_user(license: str, search: str):
+    '''Required: `license, search`'''
     logging.info(">>> " + __name__ + ":find")
-    user = await crud.find_one(term=search)
+    license = license.strip().lower()
+    user = await crud.find_license_user(license, search)
     if not user:
         raise_not_found("User not found.")
     return user
 
 
 @router.put("/{search}", response_model=User)
-async def update_user(search: str, data: UserUpdate):
-    '''
-    Required: `search`, `body`<br>
-    Search: `id`, `username`, or `email`.
-    '''
+async def update_license_user(license: str, search: str, data: UserUpdate):
+    '''Required: `license, search, data`'''
     logging.info(">>> " + __name__ + ":update")
-    user = await crud.find_one(search)
+    user = await crud.find_license_user(license, search)
     if not user:
         raise_not_found("User not found.")
     return await crud.update_one(search, data)
 
 
-@router.delete("/{search}", summary="Delete user", response_model=Msg)
-async def delete(search: str):
-    '''
-    Search: `id`, `username`, or `email`.
-    '''
+@router.delete("/{search}", response_model=Msg)
+async def delete_license_user(license: str, search: str):
+    '''Required: `license, search`'''
     logging.info(">>> " + __name__ + ":find")
-    user = await crud.find_one(search)
+    user = await crud.find_license_user(license, search)
     if not user:
         raise_not_found("User not found.")
     if user['licenseOwner']:
