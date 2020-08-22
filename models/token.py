8ce21b83d8eb797b83fc0077f53c9e3aa057a7cb
muslax <muslax@gmail.com>
2020-08-22 19:42:24 +0700

    Fix login response model

--- models/token.py
+++ models/token.py
@@ -1,10 +1,11 @@
-from typing import Optional
+from typing import Any, Optional
 from pydantic import BaseModel
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+    user: Any
 
 
 class TokenData(BaseModel):
