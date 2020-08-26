3c1b048a70bc5a4ff51587dd6156c096e41d01ce
muslax <muslax@gmail.com>
2020-08-26 13:11:45 +0700

    Update license.py

--- api/v1/license.py
+++ api/v1/license.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends
 
 from api.v1.login import get_current_active_user
 from crud import license as crud
-from crud.project import find_one as find_project
+from crud.project import find_one as find_project, find_many_by_license
 from models.license import License, LicenseUpdateSelf
 from models.user import User
 from models.project import Project
@@ -32,6 +32,12 @@ async def update_info(data: LicenseUpdateSelf, current_user: User=Depends(get_cu
     return await crud.update_one(current_user.license, data)
 
 
+@router.get("/{slug}/projects", response_model=List[Project])
+async def get_license_project(slug: str, limit: int = 20, skip: int = 0):
+    return await find_many_by_license(slug, limit, skip)
+
+
 @router.get("/{slug}/projects/{id}", response_model=Project)
 async def get_license_project(slug: str, id: str):
     return await find_project(license=slug, id=id)
+
